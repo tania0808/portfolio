5e3098d1c92b80a8fc6c8859cf812d184dcfd435
Tania <tania@MacBook-Pro-de-nico.local>
2022-07-31 18:21:40 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import ContactForm from './components/ContactForm';
+import NavList from './components/NavList';
+
 function App() {
   return (
-    <div className="App font-poppins transition-all ease-linear duration-600 ">
+    <div className="App scroll-smooth font-poppins transition-all ease-linear duration-600 ">
       <Header/>
       <About/>
       <Skills/>
       <ContactForm/>
+      <NavList className='w-full container mx-auto px-24 my-6 lg:py-0 sm:flex-col flex justify-around  text-xl items-center gap-6'/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import NavList from './NavList';
+
 export default function Header() {
 
     const [isNavOpen, setIsNavOpen] = useState(false);
@@ -18,24 +20,7 @@ export default function Header() {
                 }
             </button>
         </div>
-        <ul className={`${isNavOpen ? 'opacity-100' : 'md:opacity-0 md:-translate-y-24'} w-full container mx-auto px-24 my-6 lg:py-0 transition-all ease-linear duration-600 md:flex-col flex justify-end  text-xl items-center gap-6`}>
-            <li>
-                <a href="#skill-set">
-                    <span>Skills</span>
-                </a>
-            </li>
-            <li>
-                <a href="#projects">
-                    <span>Projects</span>
-                </a>
-            </li>
-            <li>
-                <a href="#contact-me">
-                    <span>Contact</span>
-                </a>
-            </li>
-            <button className='bg-primary text-white rounded-full py-1.5 px-4'>Contact</button>
-        </ul>
+        <NavList className={`${isNavOpen ? 'opacity-100' : 'md:opacity-0 md:-translate-y-24'} w-full container mx-auto px-24 my-6 lg:py-0 transition-all ease-linear duration-600 md:flex-col flex justify-end  text-xl items-center gap-6`}/>
     </header>
   )
 }
